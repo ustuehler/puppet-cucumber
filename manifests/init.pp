dbd9838dadc0ef257f94ed60c120a096ef17847c
Uwe Stuehler <uwe@bsdx.de>
2012-04-20 15:45:47 +0200

    Style guide and documentation changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,22 +3,21 @@
 # == Requirements
 #
 # The "gem" package provider must be working, which means that Ruby and
-# Rubygems must be set up before this class can be included.  In order
-# to achieve that, the "ruby::gems" class is included and depended upon.
-#
+# RubyGems must be set up before this class can be included.  In order
+# to achieve that, the "ruby::gems" class is required.
 class cucumber
 {
-	include ruby::gems
+  include ruby::gems
 
-	package { gherkin:
-		provider => gem,
-		ensure => installed,
-		require => Class['ruby::gems']
-	}
+  package { 'gherkin':
+    provider => gem,
+    ensure   => installed,
+    require  => Class['ruby::gems']
+  }
 
-	package { cucumber:
+	package { 'cucumber':
 		provider => gem,
-		ensure => installed,
-		require => Package[gherkin]
+		ensure   => installed,
+		require  => Package['gherkin']
 	}
 }
